72b36687eddc1a91fb8bca18f6be351dfbc2da0c
claudemahon <claudemahonjr@gmail.com>
2021-01-06 15:59:49 -0800

    last push before ad scense

--- wdgk-site/src/components/navbar.js
+++ wdgk-site/src/components/navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import 'bulma/css/bulma.css'
 import {Link} from 'gatsby'
+import '../pages/style.css'
+
 
 let Navbar = () => {
     return(
         <>
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/" className="subtitle">What Does Google Know?</Link>
+                <Link to="/" className="logo navbar-item">What Does Google Know?</Link>
             </div>
         </nav>
         </>
--- wdgk-site/src/pages/index.js
+++ wdgk-site/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import 'bulma/css/bulma.css'
 import Navbar from '../components/navbar.js'
+import './style.css'
 // import { Link } from "gatsby"
 
 
@@ -9,9 +10,9 @@ const IndexPage = () => {
     <>
     <Navbar/>
       <section className="hero is-fullheight">
-        <div className="hero-body">
-          <div className="contaier">
-            <h1 className="subtitle">What Does Google Know?</h1>
+        <div className="hero-body has-text-centered">
+          <div className="container">
+            <h1 className="title">When you scroll down, you will be greeted by ads strategically placed by Google and other ad vendors. Use this site to determine the value of using a vpn and consider the information that is benign shared without your permission.</h1>
           </div>
         </div>
       </section>
